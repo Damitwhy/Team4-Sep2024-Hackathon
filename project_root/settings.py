3357b94aa15f10eeb029faabaf88b7753b15ef45
Kenneth Cox <damitwhy@gmail.com>
2024-09-19 14:02:44 +0000

    debug set to true

--- project_root/settings.py
+++ project_root/settings.py
@@ -14,7 +14,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Other settings...
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com', '.codeinstitute-ide.net',]
 
