d7a575689287683f5313f7409731d149f55e9887
Kenneth Cox <damitwhy@gmail.com>
2024-09-19 14:23:46 +0000

    added admin views

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Instrument, Sample, AudioSetup
 
-# Register your models here.
+@admin.register(Instrument)
+class InstrumentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'description')
+    search_fields = ('name', 'type')
+    list_filter = ('type',)
+
+@admin.register(Sample)
+class SampleAdmin(admin.ModelAdmin):
+    list_display = ('note', 'instrument', 'duration')
+    search_fields = ('note', 'instrument__name')
+    list_filter = ('instrument',)
+
+@admin.register(AudioSetup)
+class AudioSetupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_instrument_count')
+    search_fields = ('name',)
+    filter_horizontal = ('instruments',)
