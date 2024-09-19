d21f6711d2a01c01704f12336794c4eb7724fbdd
Linus Johansson <golfarenlj@gmail.com>
2024-09-19 20:25:27 +0000

    Add piano in views

--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'core/home.html')
+
+def piano(request):
+    return render(request, 'core/piano.html')
