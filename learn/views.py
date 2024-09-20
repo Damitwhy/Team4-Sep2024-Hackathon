03aa521bbf90d0aaa41bccaff14974d90f7883c2
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-09-20 19:53:59 +0100

    Revert "Add: keyboard code for piano sounds"
    This reverts commit e0ddab35ef4e53377a5ff5433e0199c7a2c4e27b.

--- learn/views.py
+++ learn/views.py
@@ -2,14 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def learn(request):
-    # Define the keyboard keys for one octave
-    keyboard_keys = {
-        'C': 65, 'C#': 87, 'D': 83, 'D#': 69,
-        'E': 68, 'F': 70, 'F#': 84, 'G': 71,
-        'G#': 89, 'A': 72, 'A#': 85, 'B': 74 
-    }
-
-    # Create context dictionary with keys as values
-    context = {'keys': keyboard_keys}
-
-    return render(request, 'learn/learn.html', context)
+    return render(request, 'learn/learn.html')
