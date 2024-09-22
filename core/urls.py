7e90b33152345ac7eded8b9227bb334571c6ca61
Nonty Dazana <nontyd@gmail.com>
2024-09-22 11:03:02 +0000

    create learn.html, upload instrument images, modify css and other files to connect learn

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('piano/', views.piano, name='piano'),
     path('about/', views.about, name='about'),
+    path('learn/', views.learn, name='learn'),
     #path('projects/', views.projects, name='projects'),
     #path('contact/', views.contact, name='contact'),
     #path('summernote/', include('django_summernote.urls')),  # Include Django Summernote URLs
