3163ae74991c823283ab39973167e560dfb94191
Kenneth Cox <damitwhy@gmail.com>
2024-09-23 08:05:14 +0000

    bg color staff changed

--- project_root/settings.py
+++ project_root/settings.py
@@ -15,7 +15,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Other settings...
-DEBUG =False # os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
