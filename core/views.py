977942c29b718f4ebccc85698bbed32f6ca00a50
Linus Johansson <golfarenlj@gmail.com>
2024-09-23 10:14:15 +0000

    Cleanup

--- core/views.py
+++ core/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 from .models import Contributor
 
 
-# Create your views here.
 def home(request):
     return render(request, 'core/home.html')
+
 
 def learn(request):
     return render(request, 'core/learn.html')
 
+
 def about(request):
     contributors = Contributor.objects.all()
     return render(request, 'core/about.html', {'contributors': contributors})
-
